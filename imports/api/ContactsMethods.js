5aa9acd5ddea6653fb056f74afba26ec63a771f1
Julian Berrio <julian.berrio@MacBook-Pro.local>
2025-05-22 17:48:45 -0500

    removing contacts

--- imports/api/ContactsMethods.js
+++ imports/api/ContactsMethods.js
@@ -1,15 +1,26 @@
 import {Meteor} from 'meteor/meteor';
 import {ContactsCollection} from './ContactsCollection.js';
+import {check} from 'meteor/check';
 
 Meteor.methods({
     'contacts.insert'(name, email, imageURL){
         if(!name || !email || !imageURL){
             throw new Meteor.Error('Invalid contact', 'All fields are required');
         }
+        check(name, String);
+        check(email, String);
+        check(imageURL, String);
         ContactsCollection.insert({
             name,
             email,
             imageURL
         });
+    },
+    'contacts.remove'(contactId){
+        if(!contactId){
+            throw new Meteor.Error('Invalid contact', 'Contact ID is required');
+        }
+        check(contactId, String);
+        ContactsCollection.remove(contactId);
     }
 })
--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -5,13 +5,26 @@ export const ContactList = () => {
     const contacts = useTracker( () => {
         return  ContactsCollection.find({}).fetch();
     });
+    const deleteSubmit = (e, contactId) => {
+        e.preventDefault();
+        Meteor.call('contacts.remove', contactId, (error) => {
+            if (error) {
+                console.error('Error deleting contact:', error);
+            } else {
+                console.log('Contact deleted successfully');
+            }
+        });
+    };
     return (
         <div className='mt-10'>
             <h2 className='font-bold text-gray-500'>Contact List</h2>
             <ul className=''>
                 {contacts.map(contact => (
-                        <li className="p-2 text-white bg-black rounded-md flex gap-2 mt-2" key={contact._id}>
-                            <img className="w-12 rounded-md" src={contact.imageURL} alt="" /><p>{contact.name} - {contact.email}</p>
+                        <li className="p-2 text-white bg-black rounded-xl  mt-2 flex justify-between " key={contact._id}>
+                            <div className='flex gap-2'><img className="w-12 h-12 rounded-md " src={contact.imageURL} alt="" /><p>{contact.name} - {contact.email}</p></div>
+                            <div className='flex items-center '>
+                                <button type="button" className='text-white border border-white rounded-xl h-8 px-1 hover:bg-gray-700 ' onClick={(e) => deleteSubmit(e, contact._id)}>delete</button>
+                            </div>
                         </li>
                 ))}
             </ul>
